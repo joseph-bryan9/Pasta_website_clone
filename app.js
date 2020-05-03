3c5ae7ac8a5323208c96f1b450deacba7532eed2
Joseph <josephbryan.sena@gmail.com>
2020-05-04 00:36:19 +0800

    Pasta website - Spinner feature added.

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ function bannerTextdisplay() {
 window.onscroll = bannerTextdisplay;
 
 // Overlay Nav-menu
-// Fullscreen overlay nav-menu script
 function showNavigation() {
   document.getElementById("myNav").style.display = "block";
   document.querySelector(".container").style.display = "none";
@@ -18,3 +17,15 @@ function closeNavigation() {
   document.getElementById("myNav").style.display = "none";
   document.querySelector(".container").style.display = "block";
 }
+
+// Spinner
+document.querySelector(".nav-menu").style.background = "transparent";
+document.querySelector(".banner-section").style.visibility = "hidden";
+document.querySelector(".spinner").classList.add("spinner");
+
+setTimeout(() => {
+  document.querySelector(".nav-menu").style.backgroundImage =
+    "url('assets/sky.jpg')";
+  document.querySelector(".spinner").classList.remove("spinner");
+  document.querySelector(".banner-section").style.visibility = "visible";
+}, 3000);
